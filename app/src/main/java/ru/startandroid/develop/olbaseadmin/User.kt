f1477603278dd4746d769b5b3e5c7b681805c0d6
MassaPro <63496032+MassaPro@users.noreply.github.com>
2020-04-13 10:17:37 +0300

    Second

---
+++ app/src/main/java/ru/startandroid/develop/olbaseadmin/User.kt
@@ -0,0 +1,11 @@
+package ru.startandroid.develop.olbaseadmin
+
+class User {
+    val name = ""
+    var place = ""
+    var rating = ""
+    class Olymp {
+        val name = ""
+    }
+    var olymps: MutableList<Olymp> = mutableListOf()
+}
