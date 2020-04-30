613a21fffdbce4b2100805d8e70a15d570ebef6e
MassaPro <63496032+MassaPro@users.noreply.github.com>
2020-04-30 11:06:38 +0300

    Add search (design part)

--- app/src/main/java/ru/startandroid/develop/olbaseadmin/MainActivity.kt
+++ app/src/main/java/ru/startandroid/develop/olbaseadmin/MainActivity.kt
@@ -1,27 +1,84 @@
 package ru.startandroid.develop.olbaseadmin
 
+import android.app.SearchManager
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.view.inputmethod.InputMethodManager
+import android.widget.SearchView
 import android.widget.TextView
-import android.widget.Toast
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuItemCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.item_list_content.view.*
-import java.lang.Integer.min
+
 
 var cur: MutableList<String> = mutableListOf()
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()  {
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+
+        val manager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchItem = menu?.findItem(R.id.search)
+        val searchView = searchItem?.actionView as androidx.appcompat.widget.SearchView
+
+        searchView.setSearchableInfo(manager.getSearchableInfo((componentName)))
+
+        searchView.setOnQueryTextListener(object: androidx.appcompat.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                searchView.clearFocus()
+                searchView.setQuery("", false)
+                searchItem.collapseActionView()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
+        return true
+    }
+
+    /*override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+
+        // Get the SearchView and set the searchable configuration
+        val searchManager =
+            getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.search).actionView as SearchView
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+
+        // Do not iconify the widget;expand it by default
+        searchView.setIconifiedByDefault(false)
+        //setSearchIcons()
+        fun showInputMethod(view: View) {
+            ((Context.INPUT_METHOD_SERVICE) as InputMethodManager)?.showSoftInput(view, 1)
+        }
+
+        searchView.setOnQueryTextFocusChangeListener { view, hasFocus ->
+            if (hasFocus) {
+                showInputMethod(view.findFocus())
+            }
+        }
+
+        return true
+    }*/
+
+    /*override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu., menu)
+        return true
+    }*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,8 +90,8 @@ class MainActivity : AppCompatActivity() {
                 // Get Post object and use the values to update the UI
                 //val post = dataSnapshot.getValue<Post>()
                 // ...
-                for (i in 1..20) {
-                    cur.add(dataSnapshot.child("vector").child(i.toString()).child("content").getValue().toString())
+                for (i in dataSnapshot.child("vector").children) {
+                    cur.add(i.child("content").getValue().toString())
                 }
                 item_list.adapter?.notifyDataSetChanged()
             }
@@ -99,3 +156,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
+private fun SearchView.setOnQueryTextListener(mainActivity: MainActivity) {
+    
+}
+
+
